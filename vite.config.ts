1cecd3ff014739b19a7adcbf16e57a77cad5ac97
Riyas Kechery <riyaskechery777@gmail.com>
2026-06-05 21:57:00 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Noor-Academy-Class-Room/',
-  // ... other configs
+  plugins: [react()],
 })
